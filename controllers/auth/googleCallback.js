8094b92e1a2c2e9f468c66a81133ead716cd4385
Antor1155 <md.antor1155@gmail.com>
2024-11-29 03:26:33 +0600

    fix: googleCallback.js updated to handle existing user login with google

--- controllers/auth/googleCallback.js
+++ controllers/auth/googleCallback.js
@@ -1,16 +1,21 @@
 const {
-	generateAccessToken,
-	generateRefreshToken,
-} = require('../../utils/jwt.js');
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../../utils/jwt.js");
 
 module.exports = async (req, res) => {
-	const accessToken = generateAccessToken(req.user);
-	const refreshToken = generateRefreshToken(req.user);
+  try {
+    const accessToken = generateAccessToken(req.user);
+    const refreshToken = generateRefreshToken(req.user);
 
-	// Save refresh token in the database
-	req.user.refreshToken = refreshToken;
-	await req.user.save();
+    // Save refresh token in the database
+    req.user.refreshToken = refreshToken;
+    await req.user.save();
 
-	// res.redirect(`/?accessToken=${accessToken}&refreshToken=${refreshToken}`);
-	return res.status(200).json({ accessToken, refreshToken });
+    // res.redirect(`/?accessToken=${accessToken}&refreshToken=${refreshToken}`);
+    return res.status(200).json({ accessToken, refreshToken });
+  } catch (err) {
+    console.error("Error during Google callback:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
