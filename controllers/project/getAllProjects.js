9f6ff0b6c6830cbd8743cacdc6eb9f58460b5ff4
Antor1155 <md.antor1155@gmail.com>
2024-11-29 19:51:31 +0600

    feat: projects controller moved form image folder to project folder, /project /all created to get all projects

---
+++ controllers/project/getAllProjects.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Project = require("../../models/Project.js");
+const ImageGeneration = require("../../models/ImageGeneration.js");
+
+module.exports = async (req, res) => {
+  try {
+    const projects = await Project.find({
+      user: req?.user?._id,
+    }).populate("media");
+
+    const unassignedImages = await ImageGeneration.find({
+      user: req?.user?._id,
+      type: "Generate",
+      status: "Completed",
+      project: null,
+    });
+
+    const unassinedProject = {
+      user: req?.user?._id,
+      name: "Unassigned",
+      media: unassignedImages,
+    };
+
+    if (unassignedImages.length) projects.push(unassinedProject);
+
+    res.status(200).json(projects);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,11 @@ const express = require("express");
 const userProtect = require("../middlewares/userProtect.js");
 const router = express.Router();
 
-const addToProject = require("../controllers/image/addToProject.js");
-const getProjectById = require("../controllers/image/getProjectById.js");
+const addToProject = require("../controllers/project/addToProject.js");
+const getProjectById = require("../controllers/project/getProjectById.js");
+const getAllProjects = require("../controllers/project/getAllProjects.js");
 
+router.get("/all", userProtect, getAllProjects);
 router.get("/:id", userProtect, getProjectById);
 router.put("/", userProtect, addToProject);
 
