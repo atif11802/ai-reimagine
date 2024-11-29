4faeef69806e9468aecc3e15e712e9283f6102d2
Antor1155 <md.antor1155@gmail.com>
2024-11-30 02:46:49 +0600

    feat: /auth/upload-profile-pic : after updating profile pic to user, delete local file

--- controllers/user/updateProfilePic.js
+++ controllers/user/updateProfilePic.js
@@ -2,6 +2,44 @@ const User = require("../../models/User.js");
 const AWS = require("aws-sdk");
 const fs = require("fs");
 
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 1000; // 1 second
+
+async function deleteFileWithRetry(filePath, retries = MAX_RETRIES) {
+  for (let attempt = 1; attempt <= retries; attempt++) {
+    try {
+      await fs.promises.access(filePath); // Check if the file exists
+      await fs.promises.unlink(filePath); // Attempt to delete the file
+      console.log(`File deleted successfully: ${filePath}`);
+
+      return;
+    } catch (err) {
+      if (err.code === "EPERM") {
+        console.log(
+          `Permission error, retrying to delete file: ${filePath}, attempt: ${attempt}`
+        );
+      } else if (err.code === "ENOENT") {
+        // If file doesn't exist, no need to retry further
+        console.log(`File not found: ${filePath}`);
+        return;
+      } else {
+        console.log(
+          `Failed to delete file: ${filePath}, attempt: ${attempt}, error: ${err.message}`
+        );
+      }
+
+      if (attempt === retries) {
+        throw new Error(
+          `Failed to delete file after ${retries} attempts: ${filePath}`
+        );
+      }
+
+      // Wait for a retry delay before trying again
+      await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+    }
+  }
+}
+
 const updateProfilePic = async (req, res) => {
   try {
     AWS.config.update({
@@ -44,12 +82,14 @@ const updateProfilePic = async (req, res) => {
     delete user.password;
 
     // Return response
-    return res.status(200).json({
+    res.status(200).json({
       message: "User Profile Pic Updated Successfully",
       user: {
         ...user,
       },
     });
+
+    await deleteFileWithRetry(req.file.path);
   } catch (error) {
     return res
       .status(500)
