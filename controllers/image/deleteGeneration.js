f648c77c2ebb25b746ed2d7d55418c1ad6852d88
Antor1155 <md.antor1155@gmail.com>
2024-12-05 19:00:38 +0600

    feat: deleteGeneration.js added in /image route

---
+++ controllers/image/deleteGeneration.js
@@ -0,0 +1,47 @@
+const ImageGeneration = require("../../models/ImageGeneration.js");
+const mongoose = require("mongoose");
+const Project = require("../../models/Project.js");
+
+module.exports = async (req, res) => {
+  try {
+    const { imageGenerationId } = req.params;
+
+    if (!mongoose.isValidObjectId(imageGenerationId)) {
+      return res.status(400).json({ message: "Invalid image generated Id" });
+    }
+
+    const image = await ImageGeneration.findOne({
+      _id: imageGenerationId,
+      user: req?.user?._id,
+      type: "Generate",
+    });
+
+    if (!image) {
+      res.status(404).json({ message: "Image Not Found" });
+    }
+
+    let project = image.project;
+
+    if (project) {
+      const pj = await Project.findById(project);
+
+      const pjMedia = pj?.media.filter(
+        (item) => String(item) !== String(imageGenerationId)
+      );
+
+      pj.media = pjMedia;
+      await pj.save();
+
+      if (!pj.media?.length) {
+        const result = await Project.findByIdAndDelete(pj._id);
+      }
+    }
+
+    await ImageGeneration.findByIdAndDelete(imageGenerationId);
+
+    res.status(200).json({ message: "Image Generation Deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
