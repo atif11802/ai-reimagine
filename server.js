1ae079af33341a009f0683b5aec87813c8a1c11c
ATIF <ratul0947@gmail.com>
2024-12-05 15:22:50 +0600

    fix: hellog

--- server.js
+++ server.js
@@ -4,5 +4,7 @@ const app = require("./app.js");
 
 const port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
+
+//hello
