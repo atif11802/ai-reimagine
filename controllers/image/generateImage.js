28c75c820103ba2afb701bb52fc581aa13432249
Antor1155 <md.antor1155@gmail.com>
2024-12-06 02:12:53 +0600

    feat: bug fix: generateImage.js

--- controllers/image/generateImage.js
+++ controllers/image/generateImage.js
@@ -5,164 +5,166 @@ const callReimagine = require("../../utils/callReimagine.js");
 const getUserCredits = require("../../utils/getUserCredits.js");
 
 module.exports = async (req, res) => {
-	const {
-		solutionId,
-		maskJobId,
-		masks,
-		spaceType,
-		designTheme,
-		colorPreference,
-		materialPreference,
-		landscapingPreference,
-		maskingElement,
-		generationCount = 1,
-		additionalPrompt,
-		maskCategory,
-	} = req.body;
+  const {
+    solutionId,
+    maskJobId,
+    masks,
+    spaceType,
+    designTheme,
+    colorPreference,
+    materialPreference,
+    landscapingPreference,
+    maskingElement,
+    generationCount = 1,
+    additionalPrompt,
+    maskCategory,
+  } = req.body;
 
-	try {
-		const solution = await Solution.findOne({
-			_id: solutionId,
-			user: req.user._id,
-		});
+  try {
+    const solution = await Solution.findOne({
+      _id: solutionId,
+      user: req.user._id,
+    });
 
-		if (!solution) {
-			return res.status(404).json({ message: "Solution Not Found" });
-		}
+    if (!solution) {
+      return res.status(404).json({ message: "Solution Not Found" });
+    }
 
-		const imageUrl = solution.url;
+    const imageUrl = solution.url;
 
-		if (!imageUrl) {
-			return res.status(404).json({ message: "Image Not Found in solution" });
-		}
+    if (!imageUrl) {
+      return res.status(404).json({ message: "Image Not Found in solution" });
+    }
 
-		let mask;
-		let gettedMask = [];
+    let mask;
+    let gettedMask = [];
 
-		if (maskJobId) {
-			mask = await ImageGeneration.findOne({ jobId: maskJobId });
+    if (maskJobId) {
+      mask = await ImageGeneration.findOne({ jobId: maskJobId });
 
-			mask?.maskUrls.forEach((item) => {
-				if (maskCategory === "architectural") {
-					if (item.name === maskingElement) {
-						gettedMask.push(item.url);
-					}
-				} else {
-					gettedMask.push(item.url);
-				}
-			});
-		}
-		console.log(mask);
-		const userCredits = await getUserCredits(req.user._id);
+      mask?.maskUrls.forEach((item) => {
+        if (maskCategory === "architectural") {
+          if (item.name === maskingElement) {
+            gettedMask.push(item.url);
+          }
+        } else {
+          gettedMask.push(item.url);
+        }
+      });
+    }
+    console.log(mask);
+    const userCredits = await getUserCredits(req.user._id);
 
-		if (
-			userCredits === 0 ||
-			userCredits < process.env.CREDITS_CONSUME_PER_REQUEST
-		) {
-			return res.status(403).json({ message: "Insufficient Credit" });
-		}
+    if (
+      userCredits === 0 ||
+      userCredits < process.env.CREDITS_CONSUME_PER_REQUEST
+    ) {
+      return res.status(403).json({ message: "Insufficient Credit" });
+    }
 
-		const data = {
-			...(designTheme ? { design_theme: designTheme } : {}),
-			...(spaceType ? { space_type: spaceType } : {}),
-			...(maskCategory ? { mask_category: maskCategory } : {}),
-			...(colorPreference ? { color_preference: colorPreference } : {}),
-			...(materialPreference
-				? { material_preference: materialPreference }
-				: {}),
-			...(maskingElement ? { masking_element: maskingElement } : {}),
-			...(landscapingPreference
-				? { landscaping_preference: landscapingPreference }
-				: {}),
-			...(additionalPrompt ? { additional_prompt: additionalPrompt } : {}),
-			image_url: imageUrl,
-			mask_urls: mask?.maskUrls ? gettedMask : masks,
-			generation_count: generationCount,
-			webhook_url: `${process.env.BACKEND_URL}/api/image/webhook/generate`,
-		};
-		console.log("data: ", data);
+    const data = {
+      ...(designTheme ? { design_theme: designTheme } : {}),
+      ...(spaceType ? { space_type: spaceType } : {}),
+      ...(maskCategory ? { mask_category: maskCategory } : {}),
+      ...(colorPreference ? { color_preference: colorPreference } : {}),
+      ...(materialPreference
+        ? { material_preference: materialPreference }
+        : {}),
+      ...(maskingElement ? { masking_element: maskingElement } : {}),
+      ...(landscapingPreference
+        ? { landscaping_preference: landscapingPreference }
+        : {}),
+      ...(additionalPrompt ? { additional_prompt: additionalPrompt } : {}),
+      image_url: imageUrl,
+      mask_urls: mask?.maskUrls ? gettedMask : masks,
+      generation_count: generationCount,
+      webhook_url: `${process.env.BACKEND_URL}/api/image/webhook/generate`,
+    };
+    console.log("data: ", data);
 
-		const generateResponse = await callReimagine(
-			"/generate_image",
-			"POST",
-			data
-		);
+    const generateResponse = await callReimagine(
+      "/generate_image",
+      "POST",
+      data
+    );
 
-		let projectExist = await Project.findOne({
-			user: req.user._id,
-			name: "Unassigned",
-		});
+    let projectExist = await Project.findOne({
+      user: req.user._id,
+      name: "Unassigned",
+    });
 
-		if (!projectExist) {
-			projectExist = await Project.create({
-				user: req.user._id,
-				name: "Unassigned",
-			});
-		}
+    if (!projectExist) {
+      projectExist = await Project.create({
+        user: req.user._id,
+        name: "Unassigned",
+      });
+    }
 
-		const newJob = new ImageGeneration({
-			user: req.user._id,
-			imageUrl: imageUrl,
-			type: "Generate",
-			solutionId: solutionId,
-			jobId: generateResponse?.data?.job_id,
-			prompt: additionalPrompt,
-			others: {
-				maskCategory,
-				maskJobId,
-				spaceType,
-				designTheme,
-				colorPreference,
-				materialPreference,
-				landscapingPreference,
-				generationCount,
-			},
-			creditsUsed:
-				Number(process.env.CREDITS_CONSUME_PER_REQUEST) * generationCount,
-			status: "Processing",
-			project: projectExist?._id,
-		});
+    const newJob = new ImageGeneration({
+      user: req.user._id,
+      imageUrl: imageUrl,
+      type: "Generate",
+      solutionId: solutionId,
+      jobId: generateResponse?.data?.job_id,
+      prompt: additionalPrompt,
+      others: {
+        maskCategory,
+        maskJobId,
+        spaceType,
+        designTheme,
+        colorPreference,
+        materialPreference,
+        landscapingPreference,
+        generationCount,
+      },
+      creditsUsed:
+        Number(process.env.CREDITS_CONSUME_PER_REQUEST) * generationCount,
+      status: "Processing",
+      project: projectExist?._id,
+    });
 
-		await projectExist.media.push(newJob._id).save();
+    projectExist.media.push(newJob._id);
 
-		const response = await callReimagine("/get-space-type-list", "GET");
+    await projectExist.save();
 
-		if (spaceType && response?.status === "success") {
-			const allFields = [
-				...response?.data?.exterior_spaces,
-				...response?.data?.interior_spaces,
-			];
+    const response = await callReimagine("/get-space-type-list", "GET");
 
-			const spaceTypeName = allFields.find((obj) => {
-				if (obj[spaceType]) {
-					return obj;
-				}
-			});
+    if (spaceType && response?.status === "success") {
+      const allFields = [
+        ...response?.data?.exterior_spaces,
+        ...response?.data?.interior_spaces,
+      ];
 
-			if (spaceTypeName) {
-				newJob.others.spaceTypeName = spaceTypeName[spaceType];
-			}
-		}
+      const spaceTypeName = allFields.find((obj) => {
+        if (obj[spaceType]) {
+          return obj;
+        }
+      });
 
-		await newJob.save();
+      if (spaceTypeName) {
+        newJob.others.spaceTypeName = spaceTypeName[spaceType];
+      }
+    }
 
-		const solutionMedia = solution?.generated_image.length
-			? solution.generated_image
-			: [];
+    await newJob.save();
 
-		solutionMedia.push(newJob._id);
+    const solutionMedia = solution?.generated_image.length
+      ? solution.generated_image
+      : [];
 
-		solution.generated_image = solutionMedia;
-		await solution.save();
+    solutionMedia.push(newJob._id);
 
-		res.status(201).json({
-			message: "Image generation started",
-			jobId: generateResponse?.data?.job_id,
-			solutionId: solutionId,
-		});
-	} catch (error) {
-		if (error?.name === "AxiosError")
-			return res.status(400).json({ message: error.message });
-		res.status(500).json({ message: error.message });
-	}
+    solution.generated_image = solutionMedia;
+    await solution.save();
+
+    res.status(201).json({
+      message: "Image generation started",
+      jobId: generateResponse?.data?.job_id,
+      solutionId: solutionId,
+    });
+  } catch (error) {
+    if (error?.name === "AxiosError")
+      return res.status(400).json({ message: error.message });
+    res.status(500).json({ message: error.message });
+  }
 };
